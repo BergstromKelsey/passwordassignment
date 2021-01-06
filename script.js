c9a581831624f06c24e01e9c997fda2abd5e5955
Kelsey <BergstromKelsey@gmail.com>
2021-01-06 15:32:09 -0600

    Third Push

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
 var generateBtn = document.querySelector("#generate");
 var userPswdLen = document.querySelector("#characterAmount");
-
+var passbox = document.querySelector("#password");
 
 
 function writePassword(event) {
     event.preventDefault();
-    //console.log(userPswdLen.value)
     genPswd()
 
 }
@@ -16,59 +15,75 @@ function genPswd() {
 
     //console.log(typeof parseInt(userPswdLen.value))
     var maxpswdLen = parseInt(userPswdLen.value)
-    var num = (Math.floor(Math.random() * (maxpswdLen - 8) + 8));
-      //console.log(num)
+    // var num = (Math.floor(Math.random() * (maxpswdLen - 8) + 8));
+    //console.log(num)
     var pswdstring = ""
-    for (var i = 1; i <= num; i++) {
+    for (var i = 1; i <= maxpswdLen; i++) {
         var test = Math.floor(Math.random() * randStringArr.length)
-
         pswdstring += randStringArr[test]
-        //return pswdstring
-    }
-    console.log(pswdstring)
+        // console.log(randStringArr[test])
+     }
+    // console.log(maxpswdLen)
+    // console.log(pswdstring)
+     passbox.value= pswdstring
+     
 }
 
 function getRandomLower() {
 
     if (document.getElementById("includeLower").checked == true) {
         return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    } else {
-        return ('')
+    
+    } 
+        
+      else { 
+          return ('')
+         
+         
 
-    }
+      }
 }
 
 function getRandomUpper() {
 
     if (document.getElementById("includeUpper").checked == true) {
         return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    } else {
+    } 
+      else {
         return ('')
+        
 
-    }
+      }
 }
 
 function getRandomNumber() {
     if (document.getElementById("includeNumbers").checked == true) {
         return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-    } else {
+    } 
+      else {
         return ('')
 
-    }
+      }
 
 }
-//  console.log(getRandomNumber());
+//  console.log(getRandomNumber())
 //  console.log(getRandomUpper())
+//  console.log(getRandomLower())
+
+
+
 function getRandomSymnbol() {
     const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
     if (document.getElementById("includeSymbols").checked == true) {
         return symbols[Math.floor(Math.random() * symbols.length)];
-    } else {
+    } 
+     else {
         return ('')
+        
 
-    }
+     }
 }
-//  console.log(getRandomSymnbol())
+
 
 
 generateBtn.addEventListener("click", writePassword);
